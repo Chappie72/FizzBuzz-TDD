a721eb2b91087749570d50b780cfeeca872ed540
David Theunissen <davidtheunissen@Davids-MacBook-Pro.local>
2017-07-05 09:27:51 +1200

    FizzBuzz unit tests

--- FizzBuzz.Library/FizzBuzzService.cs
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -1,4 +1,26 @@
 namespace FizzBuzz.Library
 {
+    public class FizzBuzzService 
+    {
+        public string Print(int n)
+        {
+            if(CheckDivisableBy3(n) && CheckDivisableBy5(n)) {
+                return "FizzBuzz";
+            } else if(CheckDivisableBy3(n)) {
+                return "Fizz";
+            } else if(CheckDivisableBy5(n)){
+                return "Buzz";
+            } else {
+                return n.ToString();
+            }
+        }
 
+        private bool CheckDivisableBy3(int n) {
+            return n % 3 == 0;
+        }
+
+        private bool CheckDivisableBy5(int n) {
+            return n % 5 == 0;
+        }
+    }
 }
--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -1,11 +1,40 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 using FizzBuzz.Library;
 
 namespace FizzBuzz.Tests
 {
-    [TestClass]
     public class FizzBuzzTests
     {
+        private FizzBuzzService _fizzBuzzService;
 
+        public FizzBuzzTests()
+        {
+            _fizzBuzzService = new FizzBuzzService();
+        }
+
+        [Fact]
+        public void ShouldPrintNumber()
+        {
+            Assert.Equal("1", _fizzBuzzService.Print(1));
+        }
+
+        [Fact]
+        public void ShouldPrintFizz()
+        {
+            Assert.Equal("Fizz", _fizzBuzzService.Print(3));
+        }
+
+        [Fact]
+        public void ShouldPrintBuzz()
+        {
+            Assert.Equal("Buzz", _fizzBuzzService.Print(5));
+            Assert.Equal("Buzz", _fizzBuzzService.Print(10));
+        }
+
+        [Fact]
+        public void ShouldPrintFizzBuzz()
+        {
+            Assert.Equal("FizzBuzz", _fizzBuzzService.Print(15));
+        }
     }
 }
